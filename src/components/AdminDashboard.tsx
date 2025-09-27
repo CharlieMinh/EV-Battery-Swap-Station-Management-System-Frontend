1ec7fbd4091062357461ec64faec0ee45f2d55aa
tinacoding-me <tiennhat110505@gmail.com>
2025-09-27 22:27:02 +0700

    style(dashboard): adjust side-bar logo

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -244,10 +244,17 @@ export function AdminDashboardPage({
         <Sidebar>
           <SidebarHeader>
             <div className="flex items-center p-2">
-              <div className="inline-flex items-center justify-center w-8 h-8 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg mr-3">
-                <Zap className="w-5 h-5 text-white" />
+              <div className="inline-flex items-center justify-center w-8 h-8 mr-3">
+                <img
+                  src="src/assets/logoEV2.png "
+                  alt="FPTFAST Logo"
+                  className="w-10 h-9 rounded-lg"
+                />
               </div>
-              <span className="text-lg font-semibold">EVSwap Admin</span>
+              <div className="flex flex-col">
+                <span className="text-lg font-semibold">F P T F A S T</span>
+                <span className="text-sm font-medium text-gray-500">Admin</span>
+              </div>
             </div>
           </SidebarHeader>
           <SidebarContent>
--- src/components/StaffDashBoard.tsx
+++ src/components/StaffDashBoard.tsx
@@ -202,10 +202,17 @@ export function StaffPortalPage({ user, onLogout }: StaffPortalPageProps) {
         <Sidebar>
           <SidebarHeader>
             <div className="flex items-center p-2">
-              <div className="inline-flex items-center justify-center w-8 h-8 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg mr-3">
-                <Zap className="w-5 h-5 text-white" />
+              <div className="inline-flex items-center justify-center w-8 h-8 mr-3">
+                <img
+                  src="src/assets/logoEV2.png "
+                  alt="FPTFAST Logo"
+                  className="w-10 h-9 rounded-lg"
+                />
               </div>
-              <span className="text-lg font-semibold">EVSwap Staff</span>
+              <div className="flex flex-col">
+                <span className="text-lg font-semibold">F P T F A S T</span>
+                <span className="text-sm font-medium text-gray-500">Staff</span>
+              </div>
             </div>
           </SidebarHeader>
           <SidebarContent>
